cf708844dccdd9086d22c0f6b7bab1e49977237d
eshan-one <eone84424@gmail.com>
2025-06-30 00:39:54 +0530

    30_June_25

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -6,21 +6,26 @@ const Pizza = (props) => {
 };
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Padre Gino's"),
-    React.createElement(Pizza, {
-      name: "The Pepperoni Pizza",
-      description: "Mozerella Cheese Pepporoni",
-    }),
-    React.createElement(Pizza, {
-      name: "The Peri Peri Pizza",
-      description: "Peri Peri Sauce, paprika",
-    }),
-    React.createElement(Pizza, {
-      name: "The Pineapple Pizza",
-      description: "Pineapple slice with paprika sauce on top",
-    }),
-  ]);
+  return React.createElement(
+    "div",
+    {},
+
+    [
+      React.createElement("h1", {}, "Padre Gino's"),
+      React.createElement(Pizza, {
+        name: "The Pepperoni Pizza",
+        description: "Mozerella Cheese Pepporoni",
+      }),
+      React.createElement(Pizza, {
+        name: "The Peri Peri Pizza",
+        description: "Peri Peri Sauce, paprika",
+      }),
+      React.createElement(Pizza, {
+        name: "The Pineapple Pizza",
+        description: "Pineapple slice with paprika sauce on top",
+      }),
+    ],
+  );
 };
 
 const container = document.getElementById("root");
